591cd3698c83136029be13ac68a4841ad389ee5c
Michelle Lee <mlee1019507@gmail.com>
2025-07-28 13:46:53 -0400

    put the very basic countup(?) timer in content view

--- CapstoneProject/ContentView.swift
+++ CapstoneProject/ContentView.swift
@@ -8,16 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var startDate = Date.now
+    @State var timeElapsed: Int = 0
+    
+    @State var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("Time elapsed: \(timeElapsed) sec")
+                .onReceive(timer) {firedDate in
+                    print("timer fired")
+                    timeElapsed = Int(firedDate.timeIntervalSince(startDate))
+                }
+            Button("Pause") {
+                timer.upstream.connect().cancel()
+            }
+            Button("Resume") {
+                timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+            }
         }
-        .padding()
+        .font(.largeTitle)
     }
 }
+
 
 #Preview {
     ContentView()
