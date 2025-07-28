3959a0f6b4c1dcdf6f752d322a2d517b71221907
Michelle Lee <mlee1019507@gmail.com>
2025-07-28 13:19:24 -0400

    Initial Commit

---
+++ CapstoneProject/CapstoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneProjectApp.swift
+//  CapstoneProject
+//
+//  Created by Scholar on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
