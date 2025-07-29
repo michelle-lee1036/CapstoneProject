1afa5fe58d984f52e03635371c344aa2732c4002
Vasundhara Jagadeesan <vasundhara.jagadeesan@gmail.com>
2025-07-29 09:55:05 -0400

    Added dest1 and dest2 text fields

--- CapstoneProject/MapSlide.swift
+++ CapstoneProject/MapSlide.swift
@@ -22,36 +22,59 @@ let locations = [
 ]
 
 struct MapSlide: View {
+    //Variables
     @State private var position = MapCameraPosition.region(MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 38.91, longitude: -77.04), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
         )
     )
+    @State private var dest1 = ""
+    @State private var dest2 = ""
+    @State private var showField2 = false
+    
     var body: some View {
         Map {
             ForEach(locations, id: \.name) { location in
                 Marker(location.name, coordinate: location.coordinate)
             }
+            
         }
         VStack{
-            HStack(spacing: 50) {
-                Button("Paris") {
-                    position = MapCameraPosition.region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
-                }
-
-                Button("Tokyo") {
-                    position = MapCameraPosition.region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
+//            HStack(spacing: 50) {
+//                Button("Paris") {
+//                    position = MapCameraPosition.region(
+//                        MKCoordinateRegion(
+//                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+//                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+//                        )
+//                    )
+//                }
+//
+//                Button("Tokyo") {
+//                    position = MapCameraPosition.region(
+//                        MKCoordinateRegion(
+//                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+//                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+//                        )
+//                    )
+//                }
+//            }
+            HStack {
+                if (showField2) {
+                    TextField("Destination 2", text: $dest2)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button("Submit") {
+                        
+                    }
+                } else {
+                    TextField("Destination 1", text: $dest1)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button("Add") {
+                        showField2 = true
+                    }
                 }
             }
+            .padding()
+            
         }
         
     }
