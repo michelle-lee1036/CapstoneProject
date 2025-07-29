951ae4f882c00622ba0bba8c63e40bdcea60432b
Madison Morgan <maddismbread@gmail.com>
2025-07-29 14:40:04 -0400

    added some info about Paris and their major illnesses and dangers/ things to be aware of

---
+++ CapstoneProject/HealthInfo.swift
@@ -0,0 +1,18 @@
+//
+//  HealthInfo.swift
+//  CapstoneProject
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+struct HealthInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HealthInfo()
+}
