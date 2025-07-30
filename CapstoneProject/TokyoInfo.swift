a1cb459ec90b1ba1ecde1a0807eaac4ca8413029
Madison Morgan <maddismbread@gmail.com>
2025-07-30 14:08:25 -0400

    All the things for tokyo

--- CapstoneProject/TokyoInfo.swift
+++ CapstoneProject/TokyoInfo.swift
@@ -12,7 +12,23 @@ struct TokyoInfo: View {
         Text("Tokyo")
             .font(.title)
             .fontWeight(.bold)
-        Text("Common I")
+            .padding(.bottom, 25.0)
+        Text("Tokyo is one of Japans most infamous cities. It is known for its beautiful mix of modern and traditional architecture and amazing food. ")
+            .padding(.bottom, 23.0)
+        
+        Text("Common Illnesses")
+            .font(.title3)
+        Text("Respiratory infections: Common cold, influenza")
+        Text("Gastorintestinal  illnesses: Travelers diarrhea, stomach flu")
+            .padding(.bottom, 25.0)
+        
+        Text("Air Quality")
+            .font(.title3)
+        Text("Generally good with occasional pollen")
+            .padding(.bottom, 25.0)
+        Text("Other Things of note")
+            .font(.title3)
+        Text("Possible natrual disasters: Earthquakes, typhoons, flooding")
     }
 }
 
