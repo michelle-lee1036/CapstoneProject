db529b18e0eb19e90a7432e4408c2743028be81c
Madison Morgan <maddismbread@gmail.com>
2025-07-30 13:00:51 -0400

    Made it neater and added an info slide for tokyo

--- CapstoneProject/HealthInfo.swift
+++ CapstoneProject/HealthInfo.swift
@@ -12,22 +12,29 @@ struct HealthInfo: View {
         Text("Paris")
             .font(.title)
             .fontWeight(.bold)
-            .padding(.bottom, 350.0)
+            .padding(.bottom, 25.0)
             
         Text("Paris, the capital of France is one of Europes most popular cities. Known for its iconic landmarks, world-class art and fashion, vibrant cafe culture, and romantic atmosphere. A popular and great place to travel")
+            .padding(.bottom, 23.0)
             
         Text("Common Illness")
             .font(.title3)
         Text("Respiratory infections: Common cold, Flu, Bronchitis")
         Text("Gastrointestinal illnessnes: Travelers Diarrhea")
+            .padding(.bottom, 25.0)
+
         Text("Air Quality")
             .font(.title3)
         Text("Occsional problems with air quality")
+            .padding(.bottom, 25.0)
+
+        
         Text("Other things of note")
             .font(.title3)
         Text("Pick pocketing")
         Text("Bedbugs")
-        Text("Terrorist groups threating the city")
+        Text("Terrorist groups threating the city as of 2025")
+        
         
     }
 }
---
+++ CapstoneProject/TokyoInfo.swift
@@ -0,0 +1,21 @@
+//
+//  TokyoInfo.swift
+//  CapstoneProject
+//
+//  Created by Scholar on 7/30/25.
+//
+
+import SwiftUI
+
+struct TokyoInfo: View {
+    var body: some View {
+        Text("Tokyo")
+            .font(.title)
+            .fontWeight(.bold)
+        Text("Common I")
+    }
+}
+
+#Preview {
+    TokyoInfo()
+}
