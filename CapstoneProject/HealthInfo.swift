f6ca0c8e1f06c51618de08075ae12cf328be52f2
Madison Morgan <maddismbread@gmail.com>
2025-07-30 10:17:31 -0400

    Added info about paris, general dangers, things to look out for, and health info

--- CapstoneProject/HealthInfo.swift
+++ CapstoneProject/HealthInfo.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct HealthInfo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Paris")
+            .font(.title)
+            .fontWeight(.bold)
+        Text("Common Illness")
+            .font(.title3)
+        Text("Respiratory infections: Common cold, Flu, Bronchitis")
+        Text("Gastrointestinal illnessnes: Travelers Diarrhea")
+        Text("Air Quality")
+            .font(.title3)
+        Text("Occsional problems with air quality")
+        Text("Other things of note")
+            .font(.title3)
+        Text("Pick pocketing")
+        Text("Bedbugs")
+        Text("Terrorist groups threating the city")
+        
     }
 }
 
